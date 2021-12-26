39f3d0c43605f22137a4ea8f15cb8b67eda70c19
HackTheOxidation <tomas.hagenau@protonmail.ch>
2021-12-26 18:51:46 +0100

    Got basic get functionality working.

--- twig.go
+++ twig.go
@@ -5,52 +5,125 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"os"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func crashAndBurn(err error) {
-	fmt.Println(err)
-	os.Exit(-1)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
 }
 
 type Options struct {
-	Url string
+	Url    string
 	Method string
+	Unsafe bool
+	Output string
+}
+
+func (opts *Options) Print() {
+	fmt.Printf("Options: { \n\tURL: %s, ", opts.Url)
+	fmt.Printf("\n\tMethod: %s, ", opts.Method)
+	fmt.Printf("\n\tUnsafe: %t, ", opts.Unsafe)
+	fmt.Printf("\n\tOutput: %s\n}\n", opts.Output)
+}
+
+func parseOutput(opts *Options) {
+	if opts.Output == "" {
+		if strings.Contains(opts.Url, "/") {
+			opts.Output = strings.SplitN(opts.Url, "/", 1)[1]
+		} else {
+			opts.Output = "index.html"
+		}
+	}
 }
 
 func readArgs() (Options, error) {
-	opts := Options {}
+	opts := Options{}
 
-	flag.StringVar(&opts.Url, "U", "", "Specify URL.")
-	flag.StringVar(&opts.Method, "m", "GET", "Speci")
+	flag.StringVar(&(opts.Output), "o", "", "Specify output file name.")
+	flag.StringVar(&(opts.Method), "m", "GET", "Specify HTTP method.")
+	flag.BoolVar(&(opts.Unsafe), "unsafe", false, "Specify security of the connection.")
 
 	flag.Parse()
 
 	if !flag.Parsed() {
-		return opts, errors.New("Flags could not be parsed.")
+		return opts, errors.New("twig: ERROR - Flags could not be parsed.")
 	}
 
+	opts.Url = flag.Arg(0)
+
 	if opts.Url == "" {
-		return opts, errors.New("No URL supplied.")
+		return opts, errors.New("twig: ERROR - No URL supplied.")
 	}
-	
+
+	parseOutput(&opts)
+
 	return opts, nil
 }
 
-func main() {
+func ensureProtocol(opts *Options) {
+	if !strings.Contains(opts.Url, "http") {
+		if opts.Unsafe {
+			opts.Url = "http://" + opts.Url
+		} else {
+			opts.Url = "https://" + opts.Url
+		}
+	}
+}
+
+func getOptions() Options {
 	opts, err := readArgs()
-
 	crashAndBurn(err)
-	
-	resp, err := http.Get(opts.Url)
+	ensureProtocol(&opts)
 
+	return opts
+}
+
+func writeOutputToFile(content *string, filename string) {
+	fp, err := os.Create(filename)
 	crashAndBurn(err)
 
-	fmt.Printf("Status: %s\n", resp.Status)
-	
+	l, err := fp.WriteString(*content)
+	crashAndBurn(err)
+	fmt.Println("SUCCESS - ", l, "bytes written to file:", filename)
+	err = fp.Close()
+	crashAndBurn(err)
+}
+
+func handleGET(resp *http.Response, opts *Options) {
+	if resp.StatusCode != 200 {
+		crashAndBurn(errors.New("twig: unsuccessful request - received response with status: " + resp.Status))
+	}
 	body, err := io.ReadAll(resp.Body)
+	crashAndBurn(err)
 	content := string(body[:])
 
-	fmt.Printf("Content: %s\n", content)
+	if opts.Output != "" {
+		writeOutputToFile(&content, opts.Output)
+	} else {
+		fmt.Println(content)
+	}
+}
+
+func dispatchAndExecute(opts *Options) {
+	var resp *http.Response
+	var err error
+
+	switch strings.ToUpper(opts.Method) {
+	case "GET":
+		resp, err = http.Get(opts.Url)
+		crashAndBurn(err)
+		handleGET(resp, opts)
+	default:
+		crashAndBurn(errors.New("twig: ERROR - Unrecognized method: " + opts.Method))
+	}
+}
+
+func main() {
+	opts := getOptions()
+	dispatchAndExecute(&opts)
 }
